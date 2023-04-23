e9a673c2b4eec430e9fda454e89ad283402b3df0
Jeff H <cuk8@txstate.edu>
2023-04-23 17:49:46 -0500

    non-functional frontpage

---
+++ search_engine_frontend/src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import {FaSearch} from "react-icons/fa"
+
+export const SearchBar = () => {
+    return <div className="input-wrapper"> 
+        <FaSearch if="search-icon"/>
+        <input placeholder="Type to search.." />
+    </div>
+};
