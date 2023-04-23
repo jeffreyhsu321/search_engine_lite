2a561fcbe5357030f22dd1eb62f401968c39847e
Jeff H <cuk8@txstate.edu>
2023-04-23 17:56:18 -0500

    preliminary styling for search bar

--- search_engine_frontend/src/components/SearchBar.js
+++ search_engine_frontend/src/components/SearchBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-import {FaSearch} from "react-icons/fa"
+import {FaSearch} from "react-icons/fa";
+import "./SearchBar.css";
 
 export const SearchBar = () => {
     return <div className="input-wrapper"> 
-        <FaSearch if="search-icon"/>
+        <FaSearch id="search-icon"/>
         <input placeholder="Type to search.." />
     </div>
 };
