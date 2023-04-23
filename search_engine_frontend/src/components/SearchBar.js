778c2d4c9e7f5fa93a3d2fc481559978833d78ce
Jeff H <cuk8@txstate.edu>
2023-04-23 18:06:07 -0500

    ready for API integration

--- search_engine_frontend/src/components/SearchBar.js
+++ search_engine_frontend/src/components/SearchBar.js
@@ -1,11 +1,23 @@
 import React from "react";
+import {useState} from "react";
 
 import {FaSearch} from "react-icons/fa";
 import "./SearchBar.css";
 
 export const SearchBar = () => {
+
+    const [query, setQuery] = useState("");     // search bar input
+
+    const fetchData = (value) => {
+
+    }
+
     return <div className="input-wrapper"> 
         <FaSearch id="search-icon"/>
-        <input placeholder="Type to search.." />
+        <input 
+            placeholder="Type to search.." 
+            value={query} 
+            onChange={(e) => setQuery(e.target)}
+        />
     </div>
 };
